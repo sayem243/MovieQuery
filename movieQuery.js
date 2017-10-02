a2857c06fa746af9dff7247118c1ba5ea5b3990d
rafiqulislam2000 <1520616@iub.edu.bd>
2017-10-02 09:18:57 +0600

    task 1,2,3

--- movieQuery.js
+++ movieQuery.js
@@ -4,20 +4,23 @@ var movieList = [
   { title: "Omen : The Final Conflict", year: "1981", length: 108, rating: "6.7", genre: "horror" },
   { title: "Gladiator", year: "2000", length: 155, rating: "8.5", genre: "historical" },
   { title: "The Patriot", year: "2000", length: 165, rating: "7.1", genre: "historical" },
-  { title: "The Matrix Reloaded”, year: “2003", length: 138, rating: "7.2", genre: "sci-fi" }
+  { title: "The Matrix Reloaded”, year: “2003", length: 138, rating: "7.2", genre: "sci-fi" },
+  {title: "Titanic”, year: “1997", length: 194 , rating: "7.7", genre: "Drama"},
+ 
 ];
 
 function getMovieTitle( index ){
-  //add code
+  return movieList[index].title;
 }
 
 console.log(getMovieTitle(3));
 
 function addMovie( movie ){
-  //add code
+movieList.push(movie);
+return movieList;
 }
 
-var newMovie = {};
+var newMovie = {title:"Titanic”, year: “1997", length: 194 , rating: "7.7", genre: "Drama" };
 console.log(addMovie(newMovie));
 
 function movieByRating(){
